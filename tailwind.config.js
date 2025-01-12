34cfdb1a2b3a7fbd1285852deea52bb1bf27ddca
blackzarifa <rafaelirades@gmail.com>
2025-01-12 12:39:49 -0300

    tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: generatePrimeVueColors(),
+    },
   },
   plugins: [],
 };
+
+function generatePrimeVueColors() {
+  const colors = {};
+  const variants = [
+    '',
+    '-50',
+    '-100',
+    '-200',
+    '-300',
+    '-400',
+    '-500',
+    '-600',
+    '-700',
+    '-800',
+    '-900',
+  ];
+
+  ['primary', 'secondary', 'success', 'info', 'warning', 'danger'].forEach(color => {
+    variants.forEach(variant => {
+      colors[`${color}${variant}`] = `var(--p-${color}${variant})`;
+    });
+  });
+
+  return colors;
+}
