d5cfb9e2991c42f62c897d79336c365e5605e5d5
blackzarifa <rafaelirades@gmail.com>
2025-01-11 22:51:36 -0300

    add: styling options

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,17 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import 'primeicons/primeicons.css';
+
 const app = createApp(App);
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
 
 app.use(createPinia());
 app.use(router);
