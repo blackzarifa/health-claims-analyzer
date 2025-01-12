c39fad0485b51336d15642295f661d5e1c0093f4
blackzarifa <rafaelirades@gmail.com>
2025-01-12 11:27:17 -0300

    add: default views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'leaderboard',
+      component: () => import('@/views/LeaderboardView.vue'),
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
+      path: '/influencer/:id',
+      name: 'influencer',
+      component: () => import('@/views/InfluencerView.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/LoginView.vue'),
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: () => import('@/views/AdminView.vue'),
+      meta: { requiresAuth: true },
     },
   ],
 });
+
+// router.beforeEach((to, _from, next) => {
+//   const isAuthenticated = !!localStorage.getItem('token');
+//
+//   if (to.meta.requiresAuth && !isAuthenticated) {
+//     next({ name: 'login', query: { redirect: to.fullPath } });
+//   } else {
+//     next();
+//   }
+// });
 
 export default router;
